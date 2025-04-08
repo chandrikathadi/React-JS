b58080722032445e928690a9798af0a5f363e3dd
chandrikathadi <chandrikathadi2005@gmail.com>
2025-04-08 21:53:42 +0530

    Add files via upload

---
+++ react5.js
@@ -0,0 +1,11 @@
+
+"use client";
+import React,{Component} from "react";
+class App extends Component{
+  render(){
+    return (
+      <div>this example contains JSX</div>
+    )
+  }
+}
+export default App;
