aa01c184a14f8d3b604efee9f8d49f5ccf6c5474
chandrikathadi <chandrikathadi2005@gmail.com>
2025-04-08 21:54:01 +0530

    Add files via upload

---
+++ react6.js
@@ -0,0 +1,13 @@
+"use client";
+import React, {Component} from "react";
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1 className='hello'>Hello React</h1>
+        {/This is a comment in jsx/}
+      </div>
+    );
+  }
+}
+export default App;
